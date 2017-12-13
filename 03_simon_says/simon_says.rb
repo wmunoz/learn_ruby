7a6d0bfd105fe03538d74a4471018d672c055969
Wilmar Munoz <wmunoz@alertlogic.com>
2017-12-13 11:45:09 -0500

    Fixed simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+def echo(simon_phrase)
+  simon_phrase
+end
+
+def shout(simon_phrase)
+  simon_phrase.upcase
+end
+
+def repeat(simon_phrase, times = 2)
+  simon_phrase + (" " + simon_phrase) * ( times -1 )
+end
+
+def start_of_word(simon_phrase, number_of_letters = 1)
+  letter_position = number_of_letters - 1
+  simon_phrase[0..letter_position]
+end
+
+def first_word(simon_phrase)
+  simon_phrase.split[0]
+end
+
+def titleize(simon_phrase)
+  simon_phrase.capitalize!
+  dont_capitalize = ["and", "or", "the", "but", "over", "to", "a"]
+  phrase = simon_phrase.split.map { |word|
+    if dont_capitalize.include?(word)
+      word
+    else
+      word.capitalize
+    end
+
+  }.join(" ")
+end
