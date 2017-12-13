7e5d217418ca09643da39457b1a59b268fb3e283
Wilmar Munoz <wmunoz@alertlogic.com>
2017-12-13 15:02:19 -0500

    Fixed pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
-#write your code here
+def translate(phrase)
+  vowels = ["a", "e", "i", "o", "u"]
+  translated = phrase.split.map { |word|
+    if vowels.include?(word[0])
+      word = word + "ay"
+    elsif word[0..1] == "qu"
+      word = word[2..-1] + "quay"
+    elsif word[0..2] == "squ"
+      word = word[3..-1] + "squay"
+    else
+      while !vowels.include?word[0]
+        word = word[1..-1] + word[0]
+      end
+      word = word + "ay"
+    end
+  }.join(" ")
+end
