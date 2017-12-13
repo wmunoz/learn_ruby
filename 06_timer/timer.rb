a146f69b487f84f15add885b2f0dd3650b310c7e
Wilmar Munoz <wmunoz@alertlogic.com>
2017-12-13 16:43:37 -0500

    Fixed timer.rb timer_spec.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def time_string
+    if @seconds > 86400
+      return "Sorry, max 86400 seconds (24 hours) are allowed"
+    else
+      minutes = @seconds / 60
+      hours = minutes / 60
+      seconds_remainder = @seconds % 60
+      minutes_remainder = minutes % 60
+      time = padded(hours) + ":" + padded(minutes_remainder)+ ":" + padded(seconds_remainder)
+    end
+  end
+
+  def padded(number)
+    format('%02d', number)
+  end
 end
